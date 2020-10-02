d196cf19bec040d0cac0f6aa88170ae4dc389d71
Virunee <viravongv@gmail.com>
2020-10-02 02:07:01 +0100

    implement health and damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] AudioClip deathSFX;
+    [Range (0,1)][SerializeField] float deathVolume = 0.3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathVolume);
+        // Add particle VFX effect maybe
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,8 +6,19 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] int damage = 100;
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Attacker>() && collision.GetComponent<Health>())
+        {
+            Health health = collision.GetComponent<Health>();
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
