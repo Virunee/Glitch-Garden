c651a9330e86f59a42296a631dbc9c518bc5be68
Virunee <viravongv@gmail.com>
2020-11-12 15:06:38 +0000

    add gold display

---
+++ Assets/Scripts/GoldDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldDisplay : MonoBehaviour
+{
+    [SerializeField] int gold = 100;
+    Text goldText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goldText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        goldText.text = gold.ToString();
+    }
+
+    public void AddGold(int amountToAdd)
+    {
+        gold += amountToAdd;
+        UpdateDisplay();
+    }
+
+    public void SpendGold(int amountToSpend)
+    {
+        if(gold >= amountToSpend)
+        {
+            gold -= amountToSpend;
+            UpdateDisplay();
+        }
+    }
+}
