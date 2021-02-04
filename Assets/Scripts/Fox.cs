655722eb1978c38c6d23bd92d284a30c8e879800
Virunee <viravongv@gmail.com>
2021-02-04 23:20:42 +0000

    Add fox jump logic

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        if (otherObject.GetComponent<Defender>())
+        {
+            if (otherObject.name.Contains("Grave"))
+            {
+                GetComponent<Animator>().SetTrigger("JumpTrigger");
+            } else
+            {
+                GetComponent<Attacker>().Attack(otherObject);
+            }
+                
+        }
+    }
+}
