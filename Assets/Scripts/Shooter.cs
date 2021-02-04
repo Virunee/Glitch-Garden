451a9bc6de7d33a9214c040cabee5e3c7db68721
Virunee <viravongv@gmail.com>
2021-02-04 14:43:50 +0000

    if attacker is in same lane as shooter logic

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,50 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot!");
+            // start shooting
+        } else
+        {
+            Debug.Log("waiting...");
+            // sit and wait
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount > 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    private void SetLaneSpawner()
+    {
+        // figures out what lane the shooter is in by using the attacker spawners
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner spawner in attackerSpawners)
+        {
+            bool IsCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if(IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, gun.transform.rotation);
